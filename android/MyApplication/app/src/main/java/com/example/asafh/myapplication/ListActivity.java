e4e9f2e12c75dcc5ca2d3c39b94bb116b0b0878e
tabtale <adig@tabtale.com>
2015-01-04 17:04:51 +0200

    .

--- android/MyApplication/app/src/main/java/com/example/asafh/myapplication/ListActivity.java
+++ android/MyApplication/app/src/main/java/com/example/asafh/myapplication/ListActivity.java
@@ -4,16 +4,66 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 
 public class ListActivity extends ActionBarActivity {
 
+    ArrayList<String> animalsNameList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        network.getContests();
-        setTableContent();
+        JSONObject  appData = network.getContests();
         setContentView(R.layout.activity_list);
+
+        ListView animalList=(ListView)findViewById(R.id.listView);
+
+        animalsNameList = new ArrayList<String>();
+
+
+        Iterator<String> iter = appData.keys();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            try {
+                Object value = appData.get(key);
+            } catch (JSONException e) {
+                // Something went wrong!
+            }
+        }
+
+
+        getAnimalNames();
+
+        // Create The Adapter with passing ArrayList as 3rd parameter
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, animalsNameList);
+        // Set The Adapter
+        animalList.setAdapter(arrayAdapter);
+        // register onClickListener to handle click events on each item
+        animalList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+
+            // argument position gives the index of item which is clicked
+            public void onItemClick(AdapterView<?> arg0, View v,int position, long arg3)
+            {
+
+                String selectedAnimal=animalsNameList.get(position);
+                Toast.makeText(getApplicationContext(), "User Selected : "+selectedAnimal,   Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 
@@ -42,6 +92,13 @@ public class ListActivity extends ActionBarActivity {
     public void setTableContent(){
 
 
+    }
+
+    void getAnimalNames()
+    {
+        animalsNameList.add("מירוץ ראשון לציון");
+        animalsNameList.add("מירוץ רחובות");
+        animalsNameList.add("כלשהו שקר כלזהו");
     }
 
    }
